c0c51173afb3a45810215c69565bd20c94afdc37
ai208 <yasu6328hand@gmail.com>
2026-03-12 19:55:43 +0900

    controller 途中

--- backend/controllers/weightController.js
+++ backend/controllers/weightController.js
@@ -1,2 +1,7 @@
 // ルートから受け取ったリクエストを処理する場所
 // ここでサービスやDBを呼び出して、レスポンスを返す。
+// routine -> controller -> service 
+
+const express = require("express");
+const router = express.Router();
+const WeightService = require("../services/WeightService");
